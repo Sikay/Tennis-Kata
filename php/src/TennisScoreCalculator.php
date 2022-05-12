1f6e08da1f48ac02634275092d4bec5abda4c34e
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-05-12 14:17:23 +0200

    refactor

--- php/src/TennisScoreCalculator.php
+++ php/src/TennisScoreCalculator.php
@@ -8,34 +8,39 @@ class TennisScoreCalculator {
     {
         $score = '';
 
-        if ($player1Points === 4) {
-            $score = 'Player one win match';
-        }
-
         if ($player1Points >= 3 && $player2Points >= 3) {
             $score = $this->advantageStage($player1Points, $player2Points);
         }
 
-        return $score;
+        if (($player1Points >= 4 || $player2Points >= 4) && empty($score)) {
+            $score = $this->hasWinner($player1Points, $player2Points);
+        }
 
+        return $score;
     }
 
     private function advantageStage(int $player1Points, int $player2Points): string
     {
-        if (($player1Points - $player2Points) >= 2) {
-            return 'Player one win match';
-        }
-
-        if (($player2Points - $player1Points) >= 2) {
-            return 'Player two win match';
-        }
-
         if (($player1Points - $player2Points) == 1) {
             return 'Player one advantage';
         }
 
         if (($player2Points - $player1Points) == 1) {
             return 'Player two advantage';
+        }
+
+        return '';
+    }
+
+    private function hasWinner(int $player1Points, int $player2Points): string
+    {
+
+        if (($player1Points - $player2Points) >= 2) {
+            return 'Player one win match';
+        }
+
+        if (($player2Points - $player1Points) >= 2) {
+            return 'Player two win match';
         }
 
         return '';
